cde78ab9ea12de7605993bbc7d23edc6bcae7c4a
MasakiFujie <ffffffzzzzzeeee@gmail.com>
2022-02-17 13:56:31 +0900

    deleteボタンを作成

--- js/main.js
+++ js/main.js
@@ -16,7 +16,14 @@ const app = Vue.createApp({
             this.todos.push(todo)
             //配列に追加
             this.newItem = ''
-        }            
+        },
+        deleteItem: function (index) {
+            //一つずつ削除するから、値にユニークな番号を渡さないといけない。その為にindexを引数に追加
+            // console.log('Delete')
+            // console.log(index)
+            this.todos.splice(index, 1)
+            //削除するメソッド。第一引数が削除を始める配列index。第二引数が削除する長さ
+        }
     }
 })
 app.mount('#app')
